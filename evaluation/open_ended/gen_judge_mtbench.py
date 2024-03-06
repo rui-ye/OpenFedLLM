af1eb0ab3979e48bba5555d98c2196ff117f9672
motigrez <grdyx2138039185@126.com>
2024-03-06 18:15:32 +0800

    [ruige/evaluation] modify README.md gen_judge_mtbench.py

--- evaluation/open_ended/gen_judge_mtbench.py
+++ evaluation/open_ended/gen_judge_mtbench.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
         default="data/judge_prompts.jsonl",
         help="The file of judge prompts.",
     )
-    parser.add_argument("--judge_model", type=str, default="gpt-4")
+    parser.add_argument("--judge_model", type=str, default="gpt-4-1106-preview")
     parser.add_argument("--baseline_model", type=str, default="gpt-3.5-turbo")
     parser.add_argument(
         "--mode",
